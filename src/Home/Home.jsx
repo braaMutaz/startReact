5421b146d008a4fc6df2255c66e6c5d1bdc814d2
braaMutaz <albraamutaz.bellah@gmail.com>
2023-03-26 15:10:41 +0200

    first commit

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import img1 from '../img/d48984c2-822f-4fae-951b-e486106c3098/avataaars.svg'
 export default function Home() {
   return <>
   <section>
     <div className='w-100 vh-100 HOME d-flex justify-content-center align-items-center'>
     <div className=' body-home text-center'>
-     <img src="https://routeegypt.com/start-react/assets/img/avataaars.svg" className='w-50' alt="" />
+     <img src={img1} className='w-50' alt="" />
 
 <div className='text-center mt-3 text-light'>
 <h1>START REACT</h1>
